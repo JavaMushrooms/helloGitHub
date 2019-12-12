fa146faaabd6907a6803496b4f669588c010925f
feiyang.liu <feiyang.liu-20190704>
2019-12-12 10:04:02 +0800

    hello

---
+++ src/java/com/test/Hello.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * @author feiyang.liu
+ * @date 19-12-12 上午10:02
+ * @description
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
